c46f33ed5068ad6c3d5b8794cf341abd2d5df50c
CPHut <CPHutzky@gmail.com>
2020-05-06 16:28:11 -0400

    python fun
    made more changes

--- get-quote.py
+++ get-quote.py
@@ -3,6 +3,10 @@ f = open("quotes.txt")
 quotes = f.readlines()
 f.close()
 
-last = 13
-rnd = random.randint(0, last)
-print(quotes[rnd])
+#last = 13
+#rnd = random.randint(0, last)
+print(quotes[0])
+print(quotes[1])
+print(quotes[3])
+print(quotes[9])
+print(quotes[-1])
